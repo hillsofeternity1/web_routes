7ba27de331bd09839e916055236967a4abefa0ef
Alexandr Godless <ultradesu@hexor.ru>
2017-12-14 22:45:33 +0300

    Major improvement.

--- routing.py
+++ routing.py
@@ -16,10 +16,10 @@ _get_reles_custom = "ip rule | grep 'lookup %s'"
 
 _add_gateway_for_table = 'sudo ip route add default via %s table %s'
 _del_gateway_for_table = 'sudo ip route del default via %s table %s'
-
+_add_client_to_table = 'sudo ip rule add from %s lookup %s'
+_del_client_to_table = 'sudo ip rule del from %s lookup %s'
 
 app = Flask(__name__)
-
 
 def execute(cmd, raw=False):
     shell = subprocess.Popen(
@@ -39,19 +39,42 @@ def execute(cmd, raw=False):
 def admin():
     message = ''
     if request.method == 'POST':
-        if (request.form['gateway'] and request.form['number']):
-            try:
-                socket.inet_aton(request.form['gateway'])
-                if request.form['action'] == 'Add':
-                    output = execute((_add_gateway_for_table % (request.form['gateway'], request.form['number'])))
-                else:
-                    output = execute((_del_gateway_for_table % (request.form['gateway'], request.form['number'])))
-                if output != '':
-                    message = output
-                else:
-                    message = 'Table changed successfully.'
-            except socket.error:
-                message = 'Incorrect gateway ip.'
+        try:
+            if (request.form['gateway'] and request.form['number']):
+                try:
+                    socket.inet_aton(request.form['gateway'])
+                    if request.form['action'] == 'Add':
+                        output = execute((_add_gateway_for_table %
+                            (request.form['gateway'], request.form['number'])))
+                    else:
+                        output = execute((_del_gateway_for_table %
+                            (request.form['gateway'], request.form['number'])))
+                    if output != '':
+                        message = output
+                    else:
+                        message = 'Table changed successfully.'
+                except socket.error:
+                    message = 'Incorrect gateway ip.'
+        except:
+            pass
+        try:
+            if (request.form['from'] and request.form['action']):
+                try:
+                    host = socket.gethostbyname(request.form['from'])
+                    if request.form['action'] == 'Add':
+                        output = execute((_add_client_to_table %
+                            (host, request.form['number'])))
+                    else:
+                        output = execute((_del_client_to_table %
+                            (host, request.form['number'])))
+                    if output != '':
+                        message = output
+                    else:
+                        message = 'Rule has been added.'
+                except:
+                    message = 'Something went wrong.'
+        except:
+            pass
     routes_custom = {}
     for table in execute(_get_custom_tables):
         routes_custom[table] = []
@@ -63,7 +86,6 @@ def admin():
         for line in execute(_get_reles_custom % table):
             rules_custome[table].append(line)
 
-    print(rules_custome)
     return render_template(
         'index.html',
         custom_tables=execute(_get_custom_tables),
